5e701450950dcbfc0926bccc5ce71fe20deee0b4
Adam Bennett <nyoxidetwitter@gmail.com>
2020-04-16 09:55:37 -0400

    cleaned up

--- src/main/java/ZCW/Api/Practice/Hearthstone/controllers/CardController.java
+++ src/main/java/ZCW/Api/Practice/Hearthstone/controllers/CardController.java
@@ -23,24 +23,17 @@ public class CardController {
 
     @GetMapping("/cards/{classname}")
     public ResponseEntity<?> getClassCards(@PathVariable String classname) {
-        HttpResponse<String> response =
-                Unirest
-                .get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/" + classname)
+        HttpResponse<String> response = Unirest.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/" + classname)
                 .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
                 .header("x-rapidapi-key", "4c4e379b8cmsh5b8ee3f43ccd278p1a858cjsn82edf9884af0")
                 .asString();
-
-
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-
             List<HearthCard> cards = objectMapper
                     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                     .readValue(response.getBody(), new TypeReference<List<HearthCard>>(){});
-
             if (cards.size() > 0) { return new ResponseEntity<>(cards, HttpStatus.OK); }
             else { return new ResponseEntity<>(null, HttpStatus.NO_CONTENT); }
-
         } catch (JsonProcessingException ignored) {}
         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
