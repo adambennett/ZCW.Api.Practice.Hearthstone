9abf9675513f173f5b435a1b1a6ed943f8ff4385
Adam Bennett <nyoxidetwitter@gmail.com>
2020-04-16 09:50:28 -0400

    working api endpoint

--- src/main/java/ZCW/Api/Practice/Hearthstone/controllers/CardController.java
+++ src/main/java/ZCW/Api/Practice/Hearthstone/controllers/CardController.java
@@ -1,10 +1,17 @@
 package ZCW.Api.Practice.Hearthstone.controllers;
 
+import ZCW.Api.Practice.Hearthstone.models.*;
 import ZCW.Api.Practice.Hearthstone.services.*;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.type.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kong.unirest.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 public class CardController {
@@ -16,11 +23,26 @@ public class CardController {
 
     @GetMapping("/cards/{classname}")
     public ResponseEntity<?> getClassCards(@PathVariable String classname) {
-        HttpResponse<String> response = Unirest.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/Mage")
+        HttpResponse<String> response =
+                Unirest
+                .get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/" + classname)
                 .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
                 .header("x-rapidapi-key", "4c4e379b8cmsh5b8ee3f43ccd278p1a858cjsn82edf9884af0")
                 .asString();
-       return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+
+            List<HearthCard> cards = objectMapper
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .readValue(response.getBody(), new TypeReference<List<HearthCard>>(){});
+
+            if (cards.size() > 0) { return new ResponseEntity<>(cards, HttpStatus.OK); }
+            else { return new ResponseEntity<>(null, HttpStatus.NO_CONTENT); }
+
+        } catch (JsonProcessingException ignored) {}
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
